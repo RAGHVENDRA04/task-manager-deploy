92ef2a8ad82c76b30c7d4a9f780a499036f00321
Raghvendra Singh <108226988+RAGHVENDRA04@users.noreply.github.com>
2024-11-13 23:46:12 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://mern-task-manager-app-deploy-main.vercel.app';
+export const API_URL = 'https://task-manager-deploy-virid.vercel.app';
